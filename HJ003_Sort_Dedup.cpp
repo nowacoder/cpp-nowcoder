306f5cd606970a09b83b787598632ab79890c576
nowacoder <nowadais@outlook.com>
2020-11-06 21:26:22 -0500

    HJ003 sort and remove duplicates

---
+++ HJ003_Sort_Dedup.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x;
+    vector<int>::iterator it;
+    for (string line; getline(cin, line); ) {
+        n = stoi(line);
+        vector<int> input;
+        for (int i=0; i<n; i++) {
+            getline(cin, line);
+            x = stoi(line);
+            if (input.size()==0) input.push_back(x);
+            else if (input.size()==1) {
+                if (input[0]<x) input.push_back(x);
+                else if (input[0]>x) input.insert(input.begin(), x);
+            } else {
+                if (input[0]>x) input.insert(input.begin(), x);
+                else {
+                    it = input.begin();
+                    for (int k=0; k<input.size()-1; k++) {
+                        it++;
+                        if (input[0]<x and x<input[k+1]) {
+                            input.insert(it, x);
+                            break;
+                        }
+                    }
+                    if (input[input.size()-1]<x) input.push_back(x);
+                }
+            }
+        }
+        for (int i=0; i<input.size(); i++) cout << input[i] << ' ';
+        cout << endl;
+    }
+    return 0;
+}
