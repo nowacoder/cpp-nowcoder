85473f7bcc2904cf604afec26e9996cf6584baac
nowacoder <nowadais@outlook.com>
2020-11-06 22:21:59 -0500

    HJ4, padding string with length of 8

---
+++ HJ004_Str_Padding.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (string line; getline(cin, line);) {
+        if (line.length()==0) continue;
+        else {
+            int rows = line.length()/8+(line.length()%8!=0);
+            int pst = 0;
+            for (int i=0; i<rows; i++) {
+                for (int j=0; j<8; j++) {
+                    pst = i*8 + j;
+                    if (pst<line.length()) cout << line[pst];
+                    else cout << '0';
+                } cout << endl;
+            }
+        }
+    }
+    return 0;
+}
